40a4a0debeac15056e0d5333185afef987cb246a
Ben Cooley <BMCooley@gmail.com>
2018-02-15 09:48:21 -0500

    init RequestManager

---
+++ RequestManager.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+let outstandingRequests = 0;
+const promises = [];
+
+export default class RequestManager {
+  
+  static onRequest = () => {
+    outstandingRequests += 1;
+  }
+  static onResponse = () => {
+    setTimeout(() => {
+      if (--outstandingRequests <= 0) {
+        while (promises.length) {
+          promises.shift()('response reslove ' + outstandingRequests);
+        }
+      }
+    }, 0)
+  }
+  static afterAll = () => {
+    if (!outstandingRequests) {
+      return Promise.resolve('insta resolve ' + outstandingRequests);
+    } else {
+      return new Promise((resolve) => promises.push(resolve));
+    }
+  }
+}
+
+axios.interceptors.request.use(function (config) {
+  RequestManager.onRequest();
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+  RequestManager.onResponse();
+  return response;
+}, function (error) {
+  RequestManager.onResponse();
+  return Promise.reject(error);
+});
